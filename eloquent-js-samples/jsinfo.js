dd70f95fb4575be79202f321e9819995db3fc3e6
thrash-particle <86530281+thrash-particle@users.noreply.github.com>
2024-01-15 18:33:13 -0600

    Added JS.info practice

---
+++ eloquent-js-samples/jsinfo.js
@@ -0,0 +1,23 @@
+/*
+
+Hello, object
+importance: 5
+Write the code, one line for each action:
+
+Create an empty object user.
+Add the property name with the value John.
+Add the property surname with the value Smith.
+Change the value of the name to Pete.
+Remove the property name from the object.
+
+*/
+
+let user = {
+    name: "John",
+    surname: "Smith",
+}
+
+user.name = "Pete";
+delete user.name;
+
+console.log(user);
