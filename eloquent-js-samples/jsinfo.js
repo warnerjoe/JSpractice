6f271cf3cd5018d4926b099e4a054510a82404fb
thrash-particle <86530281+thrash-particle@users.noreply.github.com>
2024-01-17 18:49:58 -0600

    JS Info Prob 3

--- eloquent-js-samples/jsinfo.js
+++ eloquent-js-samples/jsinfo.js
@@ -41,3 +41,38 @@ let schedule = {};
 function isEmpty(obj) {
     return Object.keys(obj).length === 0;
 }
+
+/* 
+
+Sum object properties
+importance: 5
+We have an object storing salaries of our team:
+
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130
+}
+Write the code to sum all salaries and store in the variable sum. Should be 390 in the example above.
+
+If salaries is empty, then the result must be 0.
+
+*/
+
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+}
+
+function calculateSum(obj) {
+    let sum = 0;
+
+    for (let person in obj) {
+        sum += obj[person];
+    }
+
+    return sum;
+}
+
+calculateSum(salaries);
