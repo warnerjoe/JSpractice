8bb6d8b7ed1e1bd86599926d4036d90b50e03884
thrash-particle <86530281+thrash-particle@users.noreply.github.com>
2024-01-22 18:57:31 -0600

    Refactor prob 3

--- eloquent-js-samples/jsinfo.js
+++ eloquent-js-samples/jsinfo.js
@@ -67,12 +67,39 @@ let salaries = {
 
 function calculateSum(obj) {
     let sum = 0;
-
-    for (let person in obj) {
+    for (person of obj) {
         sum += obj[person];
     }
-
     return sum;
 }
 
 calculateSum(salaries);
+
+/* 
+
+Multiply numeric property values by 2
+importance: 3
+Create a function multiplyNumeric(obj) that multiplies all numeric property values of obj by 2.
+
+For instance:
+
+// before the call
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu"
+};
+
+multiplyNumeric(menu);
+
+// after the call
+menu = {
+  width: 400,
+  height: 600,
+  title: "My menu"
+};
+Please note that multiplyNumeric does not need to return anything. It should modify the object in-place.
+
+P.S. Use typeof to check for a number here.
+
+*/
