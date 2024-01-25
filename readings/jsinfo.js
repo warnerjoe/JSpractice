17af57cd759c87419dc79f603ac9f67d3daf4ebf
thrash-particle <86530281+thrash-particle@users.noreply.github.com>
2024-01-24 21:29:50 -0600

    Added JS Info Ch5Q1

--- readings/jsinfo.js
+++ readings/jsinfo.js
@@ -120,3 +120,29 @@ function multiplyNumber(obj) {
 
 multiplyNumber(menu);
 console.log(menu);
+
+/*
+Two functions – one object
+importance: 2
+Is it possible to create functions A and B so that new A() == new B()?
+
+function A() { ... }
+function B() { ... }
+
+let a = new A();
+let b = new B();
+
+alert( a == b ); // true
+If it is, then provide an example of their code.
+*/
+
+let obj = {};
+
+function A() {return obj;}
+function B() {return obj;}
+
+console.log(new A() === new B())
+
+/*
+
+*/
