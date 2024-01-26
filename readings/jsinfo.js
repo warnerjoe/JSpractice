9379b02ece5656c29a0595e2c3459a5798b3ad2e
thrash-particle <86530281+thrash-particle@users.noreply.github.com>
2024-01-25 21:31:23 -0600

    Added JSInfoC5Q2

--- readings/jsinfo.js
+++ readings/jsinfo.js
@@ -144,5 +144,18 @@ function B() {return obj;}
 console.log(new A() === new B())
 
 /*
+Create new Calculator
+importance: 5
+Create a constructor function Calculator that creates objects with 3 methods:
 
+read() prompts for two values and saves them as object properties with names a and b respectively.
+sum() returns the sum of these properties.
+mul() returns the multiplication product of these properties.
+For instance:
+
+let calculator = new Calculator();
+calculator.read();
+
+alert( "Sum=" + calculator.sum() );
+alert( "Mul=" + calculator.mul() );
 */
