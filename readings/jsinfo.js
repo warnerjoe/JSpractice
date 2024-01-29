ad82d2438f3b950a56a8be065e900198caf7c47e
thrash-particle <86530281+thrash-particle@users.noreply.github.com>
2024-01-28 22:00:45 -0600

    Chapter 5 Q 4

--- readings/jsinfo.js
+++ readings/jsinfo.js
@@ -183,3 +183,36 @@ console.log( "Sum=" + calculator.sum() );
 console.log( "Mul=" + calculator.mul() );
 */
 
+/*
+Create new Accumulator
+importance: 5
+Create a constructor function Accumulator(startingValue).
+
+Object that it creates should:
+
+Store the “current value” in the property value. The starting value is set to the argument of the constructor startingValue.
+The read() method should use prompt to read a new number and add it to value.
+In other words, the value property is the sum of all user-entered values with the initial value startingValue.
+
+Here’s the demo of the code:
+
+let accumulator = new Accumulator(1); // initial value 1
+
+accumulator.read(); // adds the user-entered value
+accumulator.read(); // adds the user-entered value
+
+alert(accumulator.value); // shows the sum of these values
+*/
+
+function Accumulator(startingValue) {
+    this.value = startingValue;
+
+    this.read() {
+        this.value += +prompt('Number?', 0);
+    }
+}
+
+let accumulator = new Accumulator(1);
+accumulator.read();
+accumulator.read();
+alert(accumulator.value);
