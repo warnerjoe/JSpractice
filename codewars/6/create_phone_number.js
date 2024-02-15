a7da5b5f243e4ab5f112b8522e94e4ea5392eea2
thrash-particle <86530281+thrash-particle@users.noreply.github.com>
2024-02-15 14:10:01 -0600

    Refactor create_phone_number

--- codewars/6/create_phone_number.js
+++ codewars/6/create_phone_number.js
@@ -20,6 +20,15 @@ MY ANSWER
 BEST ANSWER
 ************************************************************************************************
 
-
+function createPhoneNumber(numbers){
+  var format = "(xxx) xxx-xxxx";
+  
+  for(var i = 0; i < numbers.length; i++)
+  {
+    format = format.replace('x', numbers[i]);
+  }
+  
+  return format;
+}
 
 */
