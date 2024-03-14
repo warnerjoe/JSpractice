e73b9548fca12c628a5e9d8ad1cbe5d028437b5b
thrash-particle <86530281+thrash-particle@users.noreply.github.com>
2024-03-13 21:59:07 -0500

    Changed from one big file to smaller more easy to locate files

---
+++ codewars/boolean_to_string.js
@@ -0,0 +1,12 @@
+/*
+------------------------------------------------------------------------------------------------
+CONVERT A BOOLEAN TO A STRING
+------------------------------------------------------------------------------------------------
+Implement a function which convert the given boolean value into its string representation.
+Note: Only valid inputs will be given.
+My Answer / Best Answer
+*/
+
+function booleanToString(b){
+    return b.toString();
+}
