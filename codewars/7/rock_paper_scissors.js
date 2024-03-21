8bd9174f6621fe0fb0e0a74ab1c1a75ac236d9cd
thrash-particle <86530281+thrash-particle@users.noreply.github.com>
2024-03-20 22:33:22 -0500

    Refactor

--- codewars/7/rock_paper_scissors.js
+++ codewars/7/rock_paper_scissors.js
@@ -17,26 +17,12 @@ MY ANSWER
 const rps = (p1, p2) => {
     if (p1 == p2) {
       return 'Draw!';
-    } else if (p1 == 'rock') {
-      if (p2 == 'scissors') {
+    } else if ((p1 == 'rock' && p2 == 'scissors') || (p1 == 'scissors' && p2 == 'paper') || (p1 == 'paper' && p2 == 'rock')) {
         return 'Player 1 won!';
-      } else if (p2 == 'paper'){
+    } else {
         return 'Player 2 won!';
-      }
-    } else if (p1 == 'scissors'){
-      if (p2 == 'paper'){
-        return 'Player 1 won!';
-      } else if( p2 == 'rock') {
-        return 'Player 2 won!';
-      }
-    } else if (p1 == 'paper') {
-      if (p2 == 'rock') {
-        return 'Player 1 won!';
-      } else if (p2 == 'scissors') {
-        return 'Player 2 won!';
-      }
     }
-  };
+};
 
 /***********************************************************************************************
 BEST ANSWER
