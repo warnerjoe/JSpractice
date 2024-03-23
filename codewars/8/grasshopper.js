0d693982dc68b1984637e4192320790d83b46853
thrash-particle <86530281+thrash-particle@users.noreply.github.com>
2024-03-23 12:56:37 -0500

    Fake binary, grasshopper greeting, season point totals, and rental car costs added.

---
+++ codewars/8/grasshopper.js
@@ -0,0 +1,19 @@
+/***********************************************************************************************
+GRASSHOPPER GREETING    
+************************************************************************************************
+
+Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+Use conditionals to return the proper message:
+
+case	                return
+name equals owner	    'Hello boss'
+otherwise	            'Hello guest'
+
+************************************************************************************************
+MY ANSWER / BEST ANSWER
+***********************************************************************************************/
+
+function greet (name, owner) {
+    return name === owner ? 'Hello boss' : 'Hello guest';
+}
