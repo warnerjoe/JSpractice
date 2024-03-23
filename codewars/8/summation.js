4c764918d84380a3677b4bd97ea3cfdf29f6f5bc
thrash-particle <86530281+thrash-particle@users.noreply.github.com>
2024-03-22 21:57:34 -0500

    Added "summation" and "categorize new members"

---
+++ codewars/8/summation.js
@@ -0,0 +1,18 @@
+/***********************************************************************************************
+SUMMATION
+************************************************************************************************
+
+Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0. Your function only needs to return the result, what is shown between parentheses in the example below is how you reach that result and it's not part of it, see the sample tests.
+
+************************************************************************************************
+MY ANSWER / BEST ANSWER
+***********************************************************************************************/
+
+var summation = function (num) {
+    let sum = 0
+    for (let i = 0; i <= num; i++){
+      sum += i;
+    }
+    return sum;
+  }
+  
