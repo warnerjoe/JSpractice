dc8d44b8329078c1226555c19fd3b31fe44bd1f2
thrash-particle <86530281+thrash-particle@users.noreply.github.com>
2024-04-07 12:48:43 -0500

    Added dog years calculator

---
+++ codeacademy/dogyears.js
@@ -0,0 +1,22 @@
+// A variable that will change for the dog years.
+let earlyYears = 2;
+
+// Multiply early years by 10.5 and set it to itself
+earlyYears *= 10.5;
+
+// Create variable that will change
+let laterYears = myAge - 2;
+
+// Calculate the dog years for later years
+laterYears *= 4;
+
+console.log(earlyYears);
+console.log(laterYears);
+
+// Sum the years
+let myAgeInDogYears = (earlyYears + laterYears)
+
+// Store my name in lowercase letters
+let myName = ("Joe").toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
