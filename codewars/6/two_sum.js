f17aaade8e28323d0577c01f9e1cac911ae21a2c
thrash-particle <86530281+thrash-particle@users.noreply.github.com>
2024-05-31 02:29:25 -0500

    Refactor two sum

--- codewars/6/two_sum.js
+++ codewars/6/two_sum.js
@@ -23,6 +23,12 @@ MY ANSWER
 BEST ANSWER
 ************************************************************************************************
 
-
+function twoSum(numbers, target) {
+    for (var i = 0; i < numbers.length-1; i++) {
+        for (var j = i+1; j < numbers.length; j++) {
+            if (numbers[i] + numbers[j] === target) return [i, j];
+        }
+    }
+}
 
 */
