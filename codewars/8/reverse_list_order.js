3056aba3e938fcec50a26071e46b4768e529ff21
thrash-particle <86530281+thrash-particle@users.noreply.github.com>
2024-06-02 22:43:49 -0500

    Refactor reverse_list_order

--- codewars/8/reverse_list_order.js
+++ codewars/8/reverse_list_order.js
@@ -6,7 +6,7 @@ In this kata you will create a function that takes in a list and returns a list 
 
 Examples (Input -> Output)
 * [1, 2, 3, 4]  -> [4, 3, 2, 1]
-* [9, 2, 0, 7]  -> [7, 0, 2, 9]
+* [9, 2, 0, 7]  -> [7, 0, 2, 9] test
 
 ************************************************************************************************
 MY ANSWER
