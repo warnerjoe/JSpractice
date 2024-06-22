46db2054b83cf16f76b9e6b4992feffffa647eb1
thrash-particle <86530281+thrash-particle@users.noreply.github.com>
2024-06-21 23:57:07 -0500

    Refactored returning strings

--- codewars/8/returning_strings.js
+++ codewars/8/returning_strings.js
@@ -1,7 +1,10 @@
 /***********************************************************************************************
 RETURN A STRING
 ************************************************************************************************
+
 Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+// URL: 
 
 ************************************************************************************************
 MY ANSWER / BEST ANSWER
