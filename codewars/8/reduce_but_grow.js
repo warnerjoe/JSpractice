673caf3259f85b419dcbfdf708998c2e0f2e9dcd
thrash-particle <86530281+thrash-particle@users.noreply.github.com>
2024-06-23 23:58:37 -0500

    Refactored reduce but grow

--- codewars/8/reduce_but_grow.js
+++ codewars/8/reduce_but_grow.js
@@ -4,6 +4,8 @@ REDUCE BUT GROW
 
 Given a non-empty array of integers, return the result of multiplying the values together in order. 
 
+// URL: 
+
 ************************************************************************************************
 MY ANSWER
 ***********************************************************************************************/
