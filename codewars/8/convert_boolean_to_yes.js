bde129699b5e4ebb23432123532d0e36ad06cb0a
thrash-particle <86530281+thrash-particle@users.noreply.github.com>
2024-07-16 00:06:34 -0500

    Refactored convert boolean to yes

--- codewars/8/convert_boolean_to_yes.js
+++ codewars/8/convert_boolean_to_yes.js
@@ -4,6 +4,8 @@ CONVERT BOOLEAN TO YES OR NO
 
 Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
 
+// URL: 
+
 ************************************************************************************************
 MY ANSWER / BEST ANSWER
 ***********************************************************************************************/
