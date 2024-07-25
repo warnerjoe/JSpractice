52d741e19ad8e9f18e1f92067e1a08e8093d115c
thrash-particle <86530281+thrash-particle@users.noreply.github.com>
2024-07-25 00:11:49 -0500

    Refactored all star challenge 18

--- codewars/8/all_star_challenge_18.js
+++ codewars/8/all_star_challenge_18.js
@@ -1,5 +1,5 @@
 /***********************************************************************************************
-TEMPLATE
+ALL STAR CHALLENGE 18
 ************************************************************************************************
 
 Create a function that accepts a string and a single character, and returns an integer of the count of occurrences the 2nd argument is found in the first one.
@@ -15,6 +15,8 @@ str_count("", 'z'); // returns 0
 Notes
 The first argument can be an empty string
 In languages with no distinct character data type, the second argument will be a string of length 1
+
+// URL: 
 
 ************************************************************************************************
 MY ANSWER / BEST ANSWER
