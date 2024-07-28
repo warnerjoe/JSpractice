9e3095fbaf9f6416ed01403675f9ef2964a82838
thrash-particle <86530281+thrash-particle@users.noreply.github.com>
2024-07-27 23:02:06 -0500

    refactor messi goals

--- codewars/8/grasshopper_messi_goals.js
+++ codewars/8/grasshopper_messi_goals.js
@@ -1,9 +1,21 @@
 /***********************************************************************************************
-TEMPLATE
+GRASSHOPPER: MESSI GOALS FUNCTION
 ************************************************************************************************
 
+Messi is a soccer player with goals in three leagues:
 
-// URL: 
+LaLiga
+Copa del Rey
+Champions
+Complete the function to return his total number of goals in all three leagues.
+
+Note: the input will always be valid.
+
+For example:
+
+5, 10, 2  -->  17
+
+// URL: https://www.codewars.com/kata/55f73be6e12baaa5900000d4/
 
 ************************************************************************************************
 PREP
